15140bf0423f73c0ed59ff25388e7cab9a5ec2ab
folkdev <folk.development@gmail.com>
2022-04-13 15:33:01 +0200

    1-mapping-users-to-get-usernames-solution

--- 1-core-javascript-concepts/1-mapping-users-to-get-usernames/main.js
+++ 1-core-javascript-concepts/1-mapping-users-to-get-usernames/main.js
@@ -1,5 +1,5 @@
 // 1. Write code to get array of names from given array of users
-// 2. Get back only active users
+// 2. Get back only active user names
 // 3. Sort users by age descending
 const users = [
   {
@@ -21,3 +21,20 @@ const users = [
     age: 30,
   },
 ];
+
+// 1
+const names = users.map((user) => user.name);
+console.log("names", names);
+
+// 2
+const activeUsersNames = users
+  .filter((user) => user.isActive)
+  .map((user) => user.name);
+console.log("activeUsersNames", activeUsersNames);
+
+// 3
+const sortingAgeActiveUsersNames = users
+  .sort((user1, user2) => (user1.age < user2.age ? 1 : -1))
+  .filter((user) => user.isActive)
+  .map((user) => user.name);
+console.log("sortingAgeActiveUsersNames", sortingAgeActiveUsersNames);
