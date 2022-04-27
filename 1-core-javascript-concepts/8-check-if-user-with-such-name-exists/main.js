89d82c1349bf3637cd2747cc9b4aad80d7bf2f02
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-04-27 11:28:12 +0200

    check-if-user-with-such-name-exists-solution

--- 1-core-javascript-concepts/8-check-if-user-with-such-name-exists/main.js
+++ 1-core-javascript-concepts/8-check-if-user-with-such-name-exists/main.js
@@ -17,3 +17,14 @@ const users = [
     isActive: false,
   },
 ];
+
+// const isNameExists = (name, arr) => arr.map((el) => el.name).indexOf(name) >= 0;
+// const isNameExists = (name, arr) => arr.findIndex((el) => el.name === name) >= 0;
+// const isNameExists = (name, arr) => Boolean(arr.find((el) => el.name === name));
+const isNameExists = (name, arr) => arr.some((el) => el.name === name);
+
+if (isNameExists("Jack", users)) {
+  console.log("Exist");
+} else {
+  console.log("Not exist");
+}
