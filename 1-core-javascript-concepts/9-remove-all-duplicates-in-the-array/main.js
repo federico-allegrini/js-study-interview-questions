8f68e72d7fb5ed2723f800c3056afa0b2abf4c07
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-04-28 13:43:19 +0200

    remove-all-duplicates-in-the-array-solution

--- 1-core-javascript-concepts/9-remove-all-duplicates-in-the-array/main.js
+++ 1-core-javascript-concepts/9-remove-all-duplicates-in-the-array/main.js
@@ -1,1 +1,22 @@
 // Remove all duplicates in the array
+const duplicateArr = [1, 2, 2, 3, 3, 3];
+
+// Primitive way
+//* const uniqueArr = (arr) => {
+//*   const result = [];
+//*   arr.forEach((item) => {
+//*     if (!result.includes(item)) {
+//*       result.push(item);
+//*     }
+//*   });
+//*   return result;
+//* };
+
+// Functional way
+//* const uniqueArr = (arr) => arr.reduce((acc, el) => (acc.includes(el) ? acc : [...acc, el]), []);
+
+// Best way
+// Set store unique values of any type
+const uniqueArr = (arr) => [...new Set(arr)];
+
+console.log(uniqueArr(duplicateArr));
