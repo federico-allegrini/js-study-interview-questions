84e922440004e72842967b852f22df0245a8830b
folkdev <folk.development@gmail.com>
2022-04-29 10:11:54 +0200

    sorting-the-array-start

---
+++ 1-core-javascript-concepts/10-sorting-the-array/main.js
@@ -0,0 +1,8 @@
+// Sort the array of numbers
+// Sort array of objects by author's lastname
+
+const books = [
+  { name: "Harry Potter", author: "Joanne Rowling" },
+  { name: "Warcross", author: "Marie Lu" },
+  { name: "The Hunger Games", author: "Suzanne Collins" },
+];
