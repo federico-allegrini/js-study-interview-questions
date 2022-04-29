853c1e87fbde10a231be4eb82139f722b2bb624a
folkdev <folk.development@gmail.com>
2022-04-29 10:26:11 +0200

    sorting-the-array-solution

--- 1-core-javascript-concepts/10-sorting-the-array/main.js
+++ 1-core-javascript-concepts/10-sorting-the-array/main.js
@@ -1,8 +1,20 @@
 // Sort the array of numbers
-// Sort array of objects by author's lastname
+const numbers = [5, 2, 7, 9, 1, 3, 4];
+// > 0	sort b before a
+// < 0	sort a before b
+// === 0	keep original order of a and b
+numbers.sort((a, b) => a - b);
+console.log(numbers);
 
+// Sort array of objects by author's lastname
 const books = [
   { name: "Harry Potter", author: "Joanne Rowling" },
   { name: "Warcross", author: "Marie Lu" },
   { name: "The Hunger Games", author: "Suzanne Collins" },
 ];
+books.sort((book1, book2) => {
+  const authorLastName1 = book1.author.split(" ")[1];
+  const authorLastName2 = book2.author.split(" ")[1];
+  return authorLastName1 < authorLastName2 ? -1 : 1;
+});
+console.log(books);
