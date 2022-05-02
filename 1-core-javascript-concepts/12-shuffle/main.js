5d08068e3e0730181e177310f542af4dbafa6e85
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-05-02 12:55:17 +0200

    shuffle-solution

--- 1-core-javascript-concepts/12-shuffle/main.js
+++ 1-core-javascript-concepts/12-shuffle/main.js
@@ -1,1 +1,10 @@
 // Write a function which implements shuffle
+
+const shuffleItems = (items) =>
+  items
+    .map((item) => ({ sort: Math.random(), value: item })) // Create a random value for each items
+    .sort((item1, item2) => item1.sort - item2.sort) // Sort the items based on the random generated sort value
+    .map((a) => a.value); // Remove from array the sort value for get the original array of items
+
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(shuffleItems(numbers));
