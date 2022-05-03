7d49bd3f8d15d8737477f1c97f455d6a46e2bda9
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-05-03 17:01:54 +0200

    find-the-number-of-occurences-of-minumum-value-in-list-solution

--- 1-core-javascript-concepts/13-find-the-number-of-occurences-of-minumum-value-in-list/main.js
+++ 1-core-javascript-concepts/13-find-the-number-of-occurences-of-minumum-value-in-list/main.js
@@ -1,3 +1,7 @@
 // Find the number of occurences of minimum value in the list
 
 const arr = [1, 2, 3, 1, 1];
+
+const minValue = Math.min(...arr); // Find the minumum value => 1
+const minArr = arr.filter((el) => el === minValue).length; // Filter the original array and leave only the minimum number, then take the length of this new array => [1, 1, 1].length = 3
+console.log(minArr); // 3
