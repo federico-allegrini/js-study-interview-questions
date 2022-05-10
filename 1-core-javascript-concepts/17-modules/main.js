9ce4d1ee34949e48e603607559b58cfd1a42332d
folkdev <folk.development@gmail.com>
2022-05-10 17:59:04 +0200

    modules-solution-pt1

--- 1-core-javascript-concepts/17-modules/main.js
+++ 1-core-javascript-concepts/17-modules/main.js
@@ -1,3 +1,11 @@
 // Create a es6 module with function getName, getSurname and default export getFullname.
+
+import getFullname, { getName, getSurname } from "./es6.js";
+console.log(
+  getName("Federico"),
+  getSurname("Allegrini"),
+  getFullname("Federico", "Allegrini")
+);
+
 // Create the same with commonJS module
 // What is the difference?
