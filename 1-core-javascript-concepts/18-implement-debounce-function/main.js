0b1ec27ad21fcc0d35372a2cfbb0fe4d368b180e
folkdev <folk.development@gmail.com>
2022-05-12 12:44:21 +0200

    implement-debounce-function-solution

--- 1-core-javascript-concepts/18-implement-debounce-function/main.js
+++ 1-core-javascript-concepts/18-implement-debounce-function/main.js
@@ -1,1 +1,24 @@
 // Create debounce function
+
+// Useful for send to backend a limited amount of call
+// Wait a certain amount of time after every keypress to send the call
+const debounce = (func, timeout = 300) => {
+  let timer;
+  // We must return a function
+  return (...args) => {
+    clearTimeout(timer); // Abort previous setTimeout
+    timer = setTimeout(() => {
+      // The keyword "this" refers to "processChange" function
+      // The keyword "apply" bind the "func" function to "processChange" function
+      func.apply(this, args);
+    }, timeout);
+  };
+};
+
+const saveInput = (name) => console.log("saveInput", name);
+const processChange = debounce(saveInput, 2000);
+// Only console log the last once, after 2 seconds
+processChange("Foo");
+processChange("Foo");
+processChange("Foo");
+processChange("Foo");
