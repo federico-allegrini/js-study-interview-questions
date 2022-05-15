4d05103b28b25a01837ff81e52d6241036f1175d
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-05-15 10:36:50 +0200

    split-each-sentence-to-a-separate-line-solution

--- 2-working-with-dom/3-split-each-sentence-to-a-separate-line/main.js
+++ 2-working-with-dom/3-split-each-sentence-to-a-separate-line/main.js
@@ -1,1 +1,16 @@
-// Split each new sentence to a separate line in the paragraph text. A sentence can be assumed to be a string of text terminated with a period (.)
+p; // Split each new sentence to a separate line in the paragraph text. A sentence can be assumed to be a string of text terminated with a period (.)
+
+const paragraph = document.querySelector("p");
+
+// My solution
+//* paragraph.innerHTML = paragraph.innerHTML
+//*   .replaceAll("..", "$")
+//*   .split(". ")
+//*   .map((sentence) => `${sentence}.<br/>`)
+//*   .join("")
+//*   .replaceAll("$", "..");
+
+// Use only "split" and "join" with regular expressions
+// https://regexr.com/
+paragraph.innerHTML =
+  paragraph.innerHTML.split(/\.[^.|<]/).join(".</p><p>") + "</p>";
