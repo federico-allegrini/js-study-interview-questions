867b34e1797232f75977d47ead5b2c77d799a403
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-05-15 10:49:31 +0200

    event-delegation-solution

--- 2-working-with-dom/4-event-delegation/main.js
+++ 2-working-with-dom/4-event-delegation/main.js
@@ -1,1 +1,17 @@
 // Implement a click on todo item as fast as possible
+
+// Bad for performance
+// Too many listeners
+//* const items = document.querySelectorAll(".item");
+//* items.forEach((item) =>
+//*   item.addEventListener("click", () => {
+//*     console.log("You clicked on item: " + item.innerText);
+//*   })
+//* );
+
+// One single listener on parent element
+const app = document.querySelector(".todo-app");
+app.addEventListener("click", (e) => {
+  if (e.target && e.target.classList.contains("item"))
+    console.log("You clicked on item: " + e.target.innerText);
+});
