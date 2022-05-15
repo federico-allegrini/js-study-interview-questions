5f8cce56fef9a63a3818765dc823bf8491bc67c6
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-05-15 18:01:43 +0200

    xml-http-request-solution

--- 3-asynchronous-javascript/1-xml-http-request/main.js
+++ 3-asynchronous-javascript/1-xml-http-request/main.js
@@ -1,1 +1,19 @@
 // Write an example of fetching data with XMLHttpRequest.
+
+const xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://api.github.com/users/federico-allegrini/repos");
+
+xhr.send();
+
+xhr.onload = function () {
+  if (xhr.status !== 200) {
+    console.log("Error", xhr.status, xhr.statusText);
+  } else {
+    console.log("Success", xhr.response);
+  }
+};
+
+xhr.onerror = function () {
+  console.log("XHR Request Failed!");
+};
