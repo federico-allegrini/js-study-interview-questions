122890592a964bf784c5abe1f1864acb316bcd85
folkdev <folk.development@gmail.com>
2022-05-16 10:36:09 +0200

    fetch-api-solution

--- 3-asynchronous-javascript/2-fetch-api/main.js
+++ 3-asynchronous-javascript/2-fetch-api/main.js
@@ -1,1 +1,6 @@
 // Write an example of fetching data using fetch API.
+
+fetch("https://api.github.com/users/federico-allegrini/repos")
+  .then((res) => res.json())
+  .then((data) => console.log("Success", data))
+  .catch((error) => console.log("Error", error));
