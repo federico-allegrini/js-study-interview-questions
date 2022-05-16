4fead307f8c1d1f26a0bf44f7a82700654119bce
folkdev <folk.development@gmail.com>
2022-05-16 10:48:28 +0200

    basic-callback-solution

--- 3-asynchronous-javascript/3-basic-callback/main.js
+++ 3-asynchronous-javascript/3-basic-callback/main.js
@@ -1,2 +1,15 @@
 // Write an asynchronous function which executes callback after finishing it's asynchronous task
+
+const asyncFn = (callback) => {
+  window.setTimeout(() => callback("Done"), 2000);
+};
+
+asyncFn((message) => {
+  console.log("Callback", message);
+});
+
 // What problem callbacks solve?
+// 1. Make async task and wait for the result
+// 2. Inside the async function we don't know what is callback
+//    Callback can do whatever in different cases
+//    We can easily share our async function
