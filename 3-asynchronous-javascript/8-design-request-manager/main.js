dc033924de60b105e065afcf55f576f6e520fefb
folkdev <folk.development@gmail.com>
2022-05-18 11:55:39 +0200

    design-request-manager-solution

--- 3-asynchronous-javascript/8-design-request-manager/main.js
+++ 3-asynchronous-javascript/8-design-request-manager/main.js
@@ -1,1 +1,50 @@
 // Design a utility which takes URL and a value for attempts which will attempt to make a fetch request. If on failure it tries again with increasing delay for number of times which user has requested
+
+// My solution
+// const requestManager = async (url, attempts = 1) => {
+//   let retry = true;
+//   let attemptsCounter = 0;
+//   while (retry)
+//     try {
+//       attemptsCounter++;
+//       const res = await fetch(url);
+//       if (!res.ok) {
+//         throw new Error(res.status + " " + res.statusText);
+//       }
+//       const data = await res.json();
+//       console.log(data);
+//       retry = false;
+//     } catch (err) {
+//       console.log(
+//         "Error",
+//         err,
+//         "Attempt " + attemptsCounter + " of " + attempts
+//       );
+//       retry = attemptsCounter < attempts;
+//     }
+// };
+// requestManager("https://foo.com", 2);
+
+// Solution
+const requestManager = (url, options = {}, attempts = 3) => {
+  return new Promise((resolve, reject) => {
+    fetch(url, options)
+      .then(resolve)
+      .catch((error) => {
+        const isLastAttempt = attempts === 1;
+        if (isLastAttempt) {
+          return reject(error);
+        }
+        setTimeout(
+          () =>
+            requestManager(url, options, --attempts)
+              .then(resolve)
+              .catch(reject),
+          3000
+        );
+      });
+  });
+};
+requestManager("https://foo.com")
+  .then((response) => console.log("Response", response))
+  .catch((err) => console.log("Error", err));
