a6d646c0a31604bd817c2516b2f9670cbd7f0e1f
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-05-19 11:39:43 +0200

    deep-comparison-solution-pt1

--- 4-comparison-functions/2-deep-comparison/main.js
+++ 4-comparison-functions/2-deep-comparison/main.js
@@ -1,1 +1,31 @@
 // Design a deep comparison function
+
+const typeOf = (input) => {
+  const rawObject = Object.prototype.toString.call(input).toLowerCase();
+  const typeOfRegex = /\[object (.*)]/g;
+  const type = typeOfRegex.exec(rawObject)[1];
+  return type;
+};
+
+// Start from shallow comparison
+const shallowCompare = (source, target) => {
+  if (typeOf(source) !== typeOf(target)) {
+    return false;
+  }
+  if (typeOf(source) === "array") {
+    if (source.length !== target.length) {
+      return false;
+    }
+    return source.every((element, index) => element === target[index]);
+  }
+  if (typeOf(source) === "object") {
+    if (Object.keys(source).length !== Object.keys(target).length) {
+      return false;
+    }
+    return Object.keys(source).every((key) => source[key] === target[key]);
+  }
+  if (typeOf(source) === "date") {
+    return source.getTime() === target.getTime();
+  }
+  return source === target;
+};
