35cd96c24d98a8381750902193f4977501c28b62
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-05-20 12:46:24 +0200

    anagram-start

---
+++ 5-tasks-asked-only-on-interview/3-anagram/main.js
@@ -0,0 +1,8 @@
+// Write a function which checks if string is an anagram
+
+// Anagrams are words that have the same characters in the same quantity. This means that two strings are anagrams if we can rearrange one to get the other.
+// Here are some examples of words that are anagrams.
+// “listen” and “silent”
+// “rail safety” and “fairy tales”
+// “dormitory” and “dirty room”
+// “the eyes” and “they see”
