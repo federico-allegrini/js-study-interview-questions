0fef4e133905c3b621a005f8e8317c754d921547
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-05-20 12:18:48 +0200

    palindrome-solution

--- 5-tasks-asked-only-on-interview/2-palindrome/main.js
+++ 5-tasks-asked-only-on-interview/2-palindrome/main.js
@@ -1,1 +1,14 @@
 // Write a function which checks if string is a palindrome
+// Same string read from start or from end
+
+// My solution
+//* const isPalindrome = (string) =>
+//*   string
+//*     .split("")
+//*     .every((letter, index, str) => letter === str[str.length - 1 - index]);
+
+// Solution
+const isPalindrome = (str) => str === str.split("").reverse().join("");
+
+console.log(isPalindrome("fof")); // true
+console.log(isPalindrome("foo")); // false
