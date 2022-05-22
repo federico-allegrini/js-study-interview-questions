02930746df8c82a6a560a8ebda19469e61ece838
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-05-22 15:20:13 +0200

    convert-time-input-to-24-format-start

---
+++ 6-tasks-from-every-day-work/2-convert-time-input-to-24-format/main.js
@@ -0,0 +1,8 @@
+// Write a function which can convert the time input given in 12 hours format to 24 hours format
+
+// convertTo24HrsFormat("12:10AM");    // 00:10
+// convertTo24HrsFormat("5:00AM");     // 05:00
+// convertTo24HrsFormat("12:33PM");    // 12:33
+// convertTo24HrsFormat("01:59PM");    // 13:59
+// convertTo24HrsFormat("11:8PM");     // 23:08
+// convertTo24HrsFormat("10:02PM");    // 22:02
