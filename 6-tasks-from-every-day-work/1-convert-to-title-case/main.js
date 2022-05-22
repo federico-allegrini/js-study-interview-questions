9657016ca03f381f661a1f6f035f48ab599fb4eb
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-05-22 13:41:53 +0200

    convert-to-title-case-solution

--- 6-tasks-from-every-day-work/1-convert-to-title-case/main.js
+++ 6-tasks-from-every-day-work/1-convert-to-title-case/main.js
@@ -5,3 +5,34 @@
 //   titleCase(“I’m a little tea pot”) should return “I’m A Little Tea Pot”.
 //   titleCase(“sHoRt AnD sToUt”) should return “Short And Stout”.
 //   titleCase(“HERE IS MY HANDLE HERE IS MY SPOUT”) should return “Here Is My Handle Here Is My Spout”.
+
+// My solution
+//* const titleCase = (str) =>
+//*   str
+//*     .toLowerCase()
+//*     .split(" ")
+//*     .map((word) =>
+//*       [...word.split("")[0].toUpperCase(), ...word.split("").slice(1)].join("")
+//*     )
+//*     .join(" ");
+
+// Beginner
+//* const titleCase = (str) => {
+//*   const arr = str.toLowerCase().split(" ");
+//*   for (var i = 0; i < arr.length; i++) {
+//*     arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+//*   }
+//*   return arr.join(" ");
+//* };
+
+// Advanced
+const titleCase = (str) =>
+  str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+
+console.log(titleCase("I'm a little tea pot")); // I’m A Little Tea Pot
+console.log(titleCase("sHoRt AnD sToUt")); // Short And Stout
+console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT")); // Here Is My Handle Here Is My Spout
