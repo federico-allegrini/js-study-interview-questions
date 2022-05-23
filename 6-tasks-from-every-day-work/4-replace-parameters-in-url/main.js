8e459ca6d45fe333d2444846717d657198fa1036
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-05-23 14:13:00 +0200

    replace-parameters-in-url-solution

--- 6-tasks-from-every-day-work/4-replace-parameters-in-url/main.js
+++ 6-tasks-from-every-day-work/4-replace-parameters-in-url/main.js
@@ -2,6 +2,29 @@
 
 const initialUrl = "/posts/:postId/comments/:commentId";
 
+// My solution
+//* const replaceParamsInUrl = (initialUrl, params) => {
+//*   const initialUrlRoutes = initialUrl.split("/");
+//*   params.forEach((param) => {
+//*     const indexToReplace = initialUrlRoutes.findIndex(
+//*       (route) => route.replace(":", "") === param.from
+//*     );
+//*     initialUrlRoutes[indexToReplace] = param.to;
+//*   });
+//*   return initialUrlRoutes.join("/");
+//* };
+
+// Solution
+// TODO:
+// 1. Go through replacements
+// 2. Replace parts of each replacements
+// 3. Return a new string
+const replaceParamsInUrl = (url, replacements) =>
+  replacements.reduce(
+    (acc, replacement) => acc.replace(":" + replacement.from, replacement.to),
+    url
+  );
+
 const resultUrl = replaceParamsInUrl(initialUrl, [
   { from: "postId", to: "1" },
   { from: "commentId", to: "3" },
