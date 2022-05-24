a1f64892c003916a9492706a870e22152f94a6b2
folkdev <folk.development@gmail.com>
2022-05-24 11:22:48 +0200

    validation-messages-solution

--- 6-tasks-from-every-day-work/5-validation-messages/main.js
+++ 6-tasks-from-every-day-work/5-validation-messages/main.js
@@ -28,3 +28,41 @@ const backendErrors = {
 };
 
 // ["Email: Can't be blank", "Password: Must contain symbols, Must be at least 8 symbols", "passwordConfirmation: Must match with password"]
+
+// My solution
+//* const formatBackend = (errors) =>
+//*   Object.entries(errors).reduce((accErr, err) => {
+//*     return [
+//*       ...accErr,
+//*       err[0].slice(0, 1).toUpperCase() +
+//*         err[0].slice(1).toLowerCase() +
+//*         ": " +
+//*         err[1].errors.reduce(
+//*           (accMsg, msg) =>
+//*             (accMsg + (accMsg === "" ? " " : ", ") + msg.message).trim(),
+//*           ""
+//*         ),
+//*     ];
+//*   }, []);
+//* console.log(formatBackend(backendErrors));
+
+// Solution
+//* const humanReadableBackendErrors = (backendErrors) =>
+//*   Object.entries(backendErrors).map(
+//*     ([field, value]) =>
+//*       field.charAt(0).toUpperCase() +
+//*       field.slice(1).toLowerCase() +
+//*       ": " +
+//*       value.errors.map((err) => err.message).join(", ")
+//*   );
+
+// Readable solution
+const humanReadableBackendErrors = (backendErrors) =>
+  Object.entries(backendErrors).map(([field, value]) => {
+    const capitalizedField =
+      field.charAt(0).toUpperCase() + field.slice(1).toLowerCase();
+    const fieldMessages = value.errors.map((err) => err.message).join(", ");
+    return `${capitalizedField}: ${fieldMessages}`;
+  });
+
+console.log(humanReadableBackendErrors(backendErrors));
